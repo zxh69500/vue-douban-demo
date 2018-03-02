b86c8231e61fa21e39d720001390b1845694d933
zhangxuehua <zhangxuehuahz@139.com>
2018-03-02 17:15:26 +0800

    add init add tabor-item

---
+++ src/components/Tabbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="m-tabbar"><slot></slot></div>
+</template>
+<style lang="less">
+.m-tabbar{
+    position: fixed;
+    display: flex;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    overflow: hidden;
+    height: 50px;
+    background: #fff;
+    border-top: 1px solid #e4e4e4;
+}
+</style>
+<script>
+import mTabbarItem from './Tabbar-item'
+export default {
+    props:['value']
+}
+</script>
+
